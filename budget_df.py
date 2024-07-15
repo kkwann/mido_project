d478822ec4925c2788b9c86c89d16cb7b8fdd805
kkwann <40452716+kkwann@users.noreply.github.com>
2024-07-15 19:51:39 +0900

    Add files via upload

--- budget_df.py
+++ budget_df.py
@@ -76,11 +76,3 @@ def budget_df():
     
     else:
         st.warning("Please login to access this page.")
-
-
-    # st.title("지자체 세부사업별 예산서")
-    # if 'logged_in' in st.session_state and st.session_state['logged_in']:
-    #     data = get_dataframe_from_bigquery('budget', 'budget_df_today')
-    #     st.write(data)
-    # else:
-    #     st.warning("Please login to access this page.")
